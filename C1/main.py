f3634e1478bfe444839213059d08a62df9b81ebd
TWitak175224 <161207572+TWitak175224@users.noreply.github.com>
2024-02-26 10:48:52 +0100

    added C1 and C2

---
+++ C1/main.py
@@ -0,0 +1,16 @@
+import sys
+
+
+print(sys.version)
+
+
+
+# git config --listcript.
+# git config --global user.name "Name"point.
+
+
+# git config --global user.email "email"
+
+text = 'zad 1'
+
+print(text)
