050a59f57970744fecf02303264e3e0f5644e99f
TWitak175224 <161207572+TWitak175224@users.noreply.github.com>
2024-03-03 11:28:41 +0100

    edited to delete two mains

---
+++ C2/C2.py
@@ -0,0 +1,75 @@
+import sys
+
+# a = 56
+# print(type(a))
+# b = 5.5
+# print(b)
+# print(type(b))
+# zmienna_tekstowa = "Wizualizacja danych"
+# print(zmienna_tekstowa)
+# print(type(zmienna_tekstowa))
+#
+# c = a // b
+# print("dzieleie całkowite(//), czyli obcina część po przecinku")
+# print("potęgowanie roi się ** użyteczny gdy nie ułamki, albo pow() cały czas")
+# # 6**1/2 zwraca 3 bo robi w kolejości (6**1)/2 więc trzeba uzywać nawiasów 6**(1/2) da pierwiastek z 6
+# print('liczba a jest rowna {:d}, a liczba b jest rowna {:.2f}'.format(a, b))
+# a = input('Wpisz liczbe: ')
+# print(a + " wartosc ")
+# a = int(a)
+# print(a)
+# sys.stdout.write('Wprowadz cyfre: ')
+# b = sys.stdin.readline()
+# print(b + "wartosc")
+# print(type(b))
+# b = int(b)
+# print(type(b))
+#
+# lista = [5, 6.6, "asa", 56, 84, "isi", [1, 2, 3]]
+# print(lista)
+# lista.append(67)
+# print(lista)
+# lista.insert(1, 'c')
+# print(lista)
+# lista.extend("abba")
+# print(lista)
+# lista.pop()
+# print(lista)
+# lista.remove([1, 2, 3])
+# print(lista)
+# del lista[1]
+# print(lista)
+# #del lista usunie całą listę!
+# print(lista)
+# lista.reverse()
+# # lista.sort()
+# # do sorta wchodzi lista możliwa do posortowania
+#
+# slownik = {'klucz': 'wartosc', 1: 2, 'a': 3, 'b': 'a'} # wartosci kluczy unikatowe, inaczej nastapi nadpisanie
+# print(slownik['a'])
+# slownik[6] = 45
+# print(slownik)
+# print(slownik[6])
+# slownik.pop(6)
+# print(slownik)
+# print(slownik.keys())
+# print(slownik.values())
+
+a = 6
+b = 7
+if a > b:
+    print("a is greater than b")
+elif a == b:
+    print("a is equal to b")
+else:
+    print("b is greater than a")
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+if (a > b) | (c > d):
+    print(a, c)
+else:
+    print(b, d)
