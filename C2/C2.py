7dbe7ebc7a19c7d5eba0a91d4dc43ee57fab2ee4
TWitak175224 <161207572+TWitak175224@users.noreply.github.com>
2024-03-06 20:58:22 +0100

    made all tasks from lab 1 and lab 2

--- C2/C2.py
+++ C2/C2.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 # a = 56
 # print(type(a))
@@ -87,12 +88,154 @@ import sys
 #     print("koniec petli")
 #
 
-lista = [2, 3, 5, 7, 12312, 12345, 6789, 543]
-a = int(input())
-licznik = 0
-while licznik < len(lista):
-    if (a - lista[licznik])!=0:
-        break
-    licznik+= 1
+# lista = [2, 3, 5, 7, 12312, 12345, 6789, 543]
+# a = int(input())
+# licznik = 0
+# while licznik < len(lista):
+#     if (a - lista[licznik])!=0:
+#         break
+#     licznik+= 1
+# else:
+#     print('żadna z listy nie zeruje Twojej liczby')
+
+
+# zadanie 1.1
+a, b = 1, 2
+c, d = 'aaaa', 'bbbb'
+e, f = 3.14, 5.21
+print(type(a), type(b), type(c), type(d), type(e), type(f))
+
+# zadanie 1.2
+a, b = float(input('Enter first number: ')), float(input('Enter second number: '))
+c = int(input("wybierz działanie:\n1 - dodawanie,\n2- odejmowanie\n3 - mnożenie\n4 dzielenie\n"))
+if c == 1:
+    print(a + b)
+elif c == 2:
+    print(a - b)
+elif c == 3:
+    print(a * b)
+elif c == 4:
+    print(a / b)
+
+# zadanie 1.3
+a = 5
+a **= 2
+print(a)
+a = 5
+a += 1
+print(a)
+a -= 1
+print(a)
+a *= 2
+print(a)
+a /= 2
+print(a)
+a %= 2
+print(a)
+
+# zadanie 1.4
+pierwsza = math.exp(10)
+print(pierwsza)
+druga = pow(math.log((5 + pow(math.sin(8), 2)), math.exp(1)), 6)
+print(druga)
+trzecia = math.floor(3.55)
+print(trzecia)
+czwarta = math.ceil(4.80)
+print(czwarta)
+
+# zadanie 1.5
+del a, b, c, d, e, f, pierwsza, druga, trzecia, czwarta
+imie = 'TOMASZ'
+nazwisko = 'WITAK'
+
+print(imie.capitalize())
+print(nazwisko.capitalize())
+
+# zadanie 1.6
+
+piosenka = 'la la la hahah ahah'
+a = piosenka.count('la')
+print(a)
+
+# zadanie 1.7
+
+napis = 'przenosze góry i doliny'
+
+print(napis[1] + napis[len(napis) - 1])
+
+# zadanie 1.8
+print(piosenka.split(' '))
+
+# zadanie 1.9
+a = 'pioseneczka'
+b = 15.3
+c = 0xabc16
+
+print('string ' + a + ' float ' + str(b) + " hex " + str(c))
+
+# zadanie 2.1
+
+lista = ['szachy', 'siatkowka', 'lekkoatletyka']
+lista.reverse()
+lista.append('boks')
+print(lista)
+
+# zadanie 2.2
+del lista, a, b, c, piosenka, napis, imie, nazwisko
+
+slownik = {'m.in': 'między innnymi', 'pok': 'pokazane', 'np': 'na przykład'}
+
+# zadanie 2.3
+del slownik
+slownik = {'Fallout': 'RPG', 'EUIV': 'Strategia', 'Grounded': 'survival', 'Skyrim': 'RPG'}
+print(slownik)
+
+# zadanie 2.4
+a = input()
+print(a.count('a'))
+
+# zadanie 2.5
+sys.stdin.write('podaj a')
+a = int(sys.stdin.buffer.readline())
+sys.stdin.write('podaj b')
+b = int(sys.stdin.buffer.readline())
+sys.stdin.write('podaj c')
+c = int(sys.stdin.buffer.readline())
+sys.stdin.write(pow(a, b) + c)
+
+# zadanie 2.6
+a, b, c = float(input('podaj a ')), float(input('podaj b ')), float(input('podaj c '))
+if a > b:
+    if a < c:
+        print(c)
+
+    else:
+        print(a)
+elif c > b:
+    print(c)
 else:
-    print('żadna z listy nie zeruje Twojej liczby')
+    print(b)
+
+# zadanie 2.7
+lista_int, lista_float = [1, 2, 3, 4, 5], [1.2, 2.3, 3.4, 4.5, 5.6]
+for i in range(0, len(lista_int)):
+    lista_int[i] = pow(lista_int[i], 2)
+for i in range(0, len(lista_float)):
+    lista_float[i] = pow(lista_float[i], 2)
+
+# zadanie 2.8
+tablica = []
+for i in range(10):
+    a = int(input("Enter a number: "))
+    if a % 2 == 0:
+        tablica.append(a)
+
+print(tablica)
+del tablica, lista_int, lista_float, lista_float, a, b, c
+
+# zadanie 2.9
+a = int(input("Podaj liczbe: "))
+if a < 0:
+    print("pierwiastkowana liczba musi być większa od 0")
+else:
+    print(pow(a, (1 / 2)))  # zrobione na około, ponieważ python nie wyrzucał błędu
