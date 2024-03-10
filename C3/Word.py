a6766a65befa224063a0d615fa37af7f65d38c95
TWitak175224 <161207572+TWitak175224@users.noreply.github.com>
2024-03-10 12:49:34 +0100

    finished tasks,

--- C3/Word.py
+++ C3/Word.py
@@ -1,5 +1,6 @@
-import random
 import math
+import random
+
 A = [1 - x for x in range(1, 11)]
 print(A)
 B = [4 ** x for x in range(8)]
@@ -12,6 +13,7 @@ lista1 = [random.randint(0, 2000) for x in range(10)]
 lista2 = [x for x in lista1 if not (x % 2)]
 print(lista1)
 print(lista2)
+
 # zad 3
 slownik = {"ziemniaki": "kg", "jogurt": "opakowania", "mleko": "kartony", "wedlina": "plastry", "ananas": "sztuki"}
 slownik_sztuk = {key: value for key, value in slownik.items() if slownik[key] == "sztuki"}
@@ -19,7 +21,6 @@ print(slownik_sztuk)
 
 
 # zad 4
-
 def czy_trojkat_prostokatny(a, b, c):
     return (a ** 2 + b ** 2 == c ** 2) or (a ** 2 + c ** 2 == b ** 2) or (b ** 2 + c ** 2 == a ** 2)
 
@@ -46,12 +47,10 @@ def iloczyn_ciagu(a=1, b=4, ile=10):
 
 print(iloczyn_ciagu(1, 2, 3))
 
-#zad 7
-a=float(input("podaj liczbe do pierwiastkowania"))
+# zad 7
+a = float(input("podaj liczbe do pierwiastkowania"))
 try:
     a = math.sqrt(a)
     print(a)
 except ValueError:
     print("Liczba ujemna! Nie oblicze pierwiastka!")
-
-
