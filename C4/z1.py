12fa96e639f32bca65e893874e2523a53d56d06c
TWitak175224 <161207572+TWitak175224@users.noreply.github.com>
2024-03-24 12:32:55 +0100

    making trough lab

--- C4/z1.py
+++ C4/z1.py
@@ -116,3 +116,29 @@ print(a[s])
 print(a[2:7:2])
 print(a[1:])
 print(a[2:5])
+print('')
+mat = np.arange(25)
+mat = mat.reshape((5, 5))
+print(mat)
+print('')
+print(mat[1:])
+print('')
+print(mat[:, 1])
+print('')
+print(mat[:, -1:])
+print('')
+print(mat[2:5, 1:3])
+print('')
+print(mat[:, range(2, 6, 2)])
+print('')
+
+x = np.array([[0, 1, 2],
+              [3, 4, 5],
+              [6, 7, 8],
+              [9, 10, 11]])
+
+print(x)
+rows = np.array([[0, 0], [3, 3]])
+cols = np.array([[0, 2], [0, 2]])
+y = x[rows, cols]
+print(y)
